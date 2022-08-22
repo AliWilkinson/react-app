d9370fa0987247b740d7150b045563a9fea6cbe3
AliWilkinson <96079887+AliWilkinson@users.noreply.github.com>
2022-08-22 20:09:11 +0100

    Update CSS

--- src/currentWeather.js
+++ src/currentWeather.js
@@ -4,19 +4,16 @@ import WeatherIcon from "./weatherIcon";
 
 export default function CurrentWeather(props) {
   return (
-    <div>
-      <h1 id="city">{props.weather.city}</h1>
-      <div className="row">
-        <div className="col-6">
-          <WeatherIcon code={props.weather.icon} size={90} />
-        </div>
-        <div className="col-6">
-          <h2>
-            {props.weather.temperature}
-            <span className="units"> &deg;C</span>
-          </h2>
-        </div>
+    <div className="currentWeather">
+      <h1 id="city">
+        {props.weather.city} {props.weather.temperature}
+        <span className="units"> &deg;C</span>
+      </h1>
+
+      <div className=" icon">
+        <WeatherIcon code={props.weather.icon} size={90} />
       </div>
+
       <h3 id="description">{props.weather.description}</h3>
     </div>
   );
